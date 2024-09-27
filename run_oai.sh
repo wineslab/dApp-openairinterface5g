0f1bd004d3343472ed737022f9150a3a2a94f6ca
Andrea Lacava <lacava.a@northeastern.edu>
2024-09-27 18:34:05 +0000

    change configuration for OTA

--- run_oai.sh
+++ run_oai.sh
@@ -3,19 +3,25 @@
 OAI_CONFIG_DIR="../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/"
 
 # x310
-# OAI_CONFIG_FILE="gnb.band78.sa.fr1.106PRB.usrpx300.conf"
+OAI_CONFIG_FILE="gnb.band78.sa.fr1.106PRB.usrpx300.conf"
 
 # b210
-OAI_CONFIG_FILE="gnb.sa.band78.fr1.106PRB.usrpb210.conf"
+# OAI_CONFIG_FILE="gnb.sa.band78.fr1.106PRB.usrpb210.conf"
+
+# Custom configuration
+# OAI_CONFIG_FILE="gNB.conf"
+# OAI_CONFIG_DIR="../../../"
 
 source oaienv
 cd ./cmake_targets/ran_build/build
 
 # x310
-# ./nr-softmodem -O ${OAI_CONFIG_DIR}${OAI_CONFIG_FILE} --gNBs.[0].min_rxtxtime 6 --sa --usrp-tx-thread-config 1  --usrp-args "addr=192.168.10.58" --T_stdout  2
+./nr-softmodem -O ${OAI_CONFIG_DIR}${OAI_CONFIG_FILE} --gNBs.[0].min_rxtxtime 6 --sa --usrp-tx-thread-config 1  --T_stdout 2 --gNBs.[0].do_SRS 0
 
 # b210
-./nr-softmodem -O ${OAI_CONFIG_DIR}${OAI_CONFIG_FILE} --gNBs.[0].min_rxtxtime 6 --sa --usrp-tx-thread-config 1 -E  --T_stdout  2
+# ./nr-softmodem -O ${OAI_CONFIG_DIR}${OAI_CONFIG_FILE} --gNBs.[0].min_rxtxtime 6 --sa --usrp-tx-thread-config 1 -E  --T_stdout  2
 
 # rfsim
 # ./nr-softmodem -O  ${OAI_CONFIG_DIR}${OAI_CONFIG_FILE}  --gNBs.[0].min_rxtxtime 6  --sa --rfsim --T_stdout 2
+
+cd -
